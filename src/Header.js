8c09ad653d02f8f6a505f0333d7e4dccd02a8017
quietstrawberry <40902988+quietstrawberry@users.noreply.github.com>
2020-12-31 17:31:07 -0500

    Started React file, and element placement

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css"
+
+export default function Header (){
+    return(
+    <div>
+        <h2 className="Header">Sarah Lane'e Studios</h2>
+       
+        <a href="null"  className=" Link">Portfolio</a>
+        <a href="null"  className=" Link">Contact</a> 
+        <a href="null" className="Link" >About</a>
+        <a href="null" className="Link" >Books</a>
+    </div>
+    );
+}
