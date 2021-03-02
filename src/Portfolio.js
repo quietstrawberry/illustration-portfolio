eb31e1ced6c889de08d6e1efa3fe11c3284574cd
quietstrawberry <40902988+quietstrawberry@users.noreply.github.com>
2021-03-02 12:54:48 -0500

    Adding portfolio example elements

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -5,6 +5,12 @@ import "./Portfolio.css";
 export default function Portfolio (){
 
     return(
-        <img src="./MooseYeti1.jpg" alt="Moose Yeti 1"></img>
-    )
+        <div className="container">
+        
+        
+        <img className="img2" src="./MooseYeti2.jpg" alt="Moose Yeti 2"></img>
+        <img className="img1" src="./MooseYeti1.jpg" alt="Moose Yeti 1"></img>
+        <img className="img3" src="./MooseYeti3.jpg" alt="Moose Yeti 3"></img>
+        </div>
+        )
 }
